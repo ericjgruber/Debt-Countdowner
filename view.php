8b557c09a36b1b46df3af9aefbb3ef7b40be35c7
Eric J Gruber <Eric.Gruber@vml.com>
2017-01-28 15:26:16 -0600

    Add a view file to see the database records

--- view.php
+++ view.php
@@ -1,7 +1,28 @@
+<?php include('theme/header.php'); ?>
+
 <?php
-/**
- * Created by PhpStorm.
- * User: ericgruber
- * Date: 1/28/17
- * Time: 3:02 PM
- */
+
+include('dbconnect.php');
+
+$sql = "SELECT date, latest_amount FROM small_victories ORDER BY latest_amount DESC;";
+$result = mysqli_query($connection, $sql);
+?>
+
+<div class="row">
+  <div class="small-6 small-centered columns latest-amounts">
+  <?php
+  if (mysqli_num_rows($result) > 0) {
+    while($row = mysqli_fetch_assoc($result)) {
+      echo "Date: " . $row['date'] . " - Amount: " . $row['latest_amount'] . "<br>";
+    }
+  } else {
+    echo "There are no amounts in the database yet.";
+  }
+
+  mysqli_close($connection);
+  ?>
+  </div>
+</div>
+
+<?php include('theme/footer.php'); ?>
+
