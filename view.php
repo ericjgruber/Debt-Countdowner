3d575991da71eefeefc69e15a30456ac4678c83e
Eric J Gruber <Eric.Gruber@vml.com>
2017-01-31 21:37:27 -0600

    Move view into a table and change the order of the sort

--- view.php
+++ view.php
@@ -4,23 +4,33 @@
 
 include('dbconnect.php');
 
-$sql = "SELECT date, latest_amount FROM small_victories ORDER BY latest_amount DESC;";
+$sql = "SELECT date, latest_amount FROM small_victories ORDER BY latest_amount ASC;";
 $result = mysqli_query($connection, $sql);
 ?>
 
 <div class="row">
   <div class="small-6 small-centered columns latest-amounts">
-  <?php
-  if (mysqli_num_rows($result) > 0) {
-    while($row = mysqli_fetch_assoc($result)) {
-      echo "Date: " . $row['date'] . " - Amount: " . $row['latest_amount'] . "<br>";
-    }
-  } else {
-    echo "There are no amounts in the database yet.";
-  }
+    <table class="hover">
+      <thead>
+      <tr>
+        <th width="200">Payment Received</th>
+        <th width="200">Remaining Debt</th>
+      </tr>
+      </thead>
+      <tbody>
+      <?php
+      if (mysqli_num_rows($result) > 0) {
+        while($row = mysqli_fetch_assoc($result)) {
+          echo "<tr><td>" . $row['date'] . "</td><td>" . $row['latest_amount'] . "</td></tr>";
+        }
+      } else {
+        echo "There are no amounts in the database yet.";
+      }
+      mysqli_close($connection);
+      ?>
+      </tbody>
+    </table>
 
-  mysqli_close($connection);
-  ?>
   </div>
 </div>
 
